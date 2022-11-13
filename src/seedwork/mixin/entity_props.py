cc7155434ff10409cd0759e2bcc0b876a282110c
olatejulian <julian.lf.olate@gmail.com>
2022-11-13 15:37:51 -0300

    test(mixin): test entity props mixin

--- src/seedwork/mixin/entity_props.py
+++ src/seedwork/mixin/entity_props.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 from pydantic import Field
+from pendulum import now
 
 from base import BaseMixin
 
 
 class EntityPropsMixin(BaseMixin):
     id: UUID = Field(default_factory=uuid4, allow_mutation=False)
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = Field(default_factory=now, allow_mutation=False)
+    updated_at: datetime = Field(default_factory=now)
 
     class Config:
-        allow_population_by_field_name = True
         validate_assignment = True
         exclude = ["__props", "__notification", "__events"]
         json_encoders = {datetime: lambda v: v.timestamp()}
